68fe42bb513a11d46271abcbe79fec83a3c8100a
griyalalita-design <griya.lalita@ninjavan.co>
2026-06-02 14:47:45 +0700

    Update print statements and add PODA processing

--- pipeline/jobs/day10_rdo.py
+++ pipeline/jobs/day10_rdo.py
@@ -13,7 +13,16 @@ def run():
 
     print("===== Ambil data dari RDO done =====")
 
-    print("===== Mulai input ke Tracker dulu ya =====")
+    print("====== Kita Kerjain Day 10 Buat PODA ya =========")
+
+    df_poda_agg = read_sheet(
+        GSHEET["poda_agg"]["sheet_id"],
+        GSHEET["poda_agg"]["tabs"]["main"]
+    )
+
+    print("===== Ambil data dari PODA done =====")
+
+    print("===== Mulai input RDO ke Tracker dulu ya =====")
     write_sheet(
         spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
         sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_otif"],
@@ -49,7 +58,46 @@ def run():
         start_cell="I5",
         include_header=False
     )
-    print("===== Done Input ke Tracker =====")
+    print("===== Done Input RDO ke Tracker =====")
+
+
+    print("===== Mulai input PODA ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_int_comp"],
+        df=df_poda_agg,
+        start_cell="P5",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_int_comp"],
+        df=df_poda_agg,
+        start_cell="Z5",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_int_comp"],
+        df=df_poda_agg,
+        start_cell="P5",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_int_comp"],
+        df=df_poda_agg,
+        start_cell="P5",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_int_comp"],
+        df=df_poda_agg,
+        start_cell="P5",
+        include_header=False
+    )
+    print("===== Done Input PODA ke Tracker =====")
 
 
 
