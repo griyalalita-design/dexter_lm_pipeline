e20766ae111ec3c60725938228f2649640183aa3
griyalalita-design <griya.lalita@ninjavan.co>
2026-06-02 15:51:41 +0700

    Update and rename day10_rdo.py to day10_external_data.py

--- pipeline/jobs/day10_external_data.py
+++ pipeline/jobs/day10_external_data.py
@@ -21,6 +21,14 @@ def run():
     )
 
     print("===== Ambil data dari PODA done =====")
+    print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
+
+    df_over_mp_fte = read_sheet(
+        GSHEET["over_mp_fte"]["sheet_id"],
+        GSHEET["over_mp_fte"]["tabs"]["main"]
+    )
+
+    print("===== Ambil data dari OVER MP done =====")
 
     print("===== Mulai input RDO ke Tracker dulu ya =====")
     write_sheet(
@@ -98,6 +106,44 @@ def run():
         include_header=False
     )
     print("===== Done Input PODA ke Tracker =====")
+
+    print("===== Mulai input OVER MP ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=Falsese
+    )
+    print("===== Done Input OVER MP ke Tracker =====")
 
 
 
