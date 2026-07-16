11abc509e827e40960323448230e78ed39aa0f6c
griyalalita-design <griya.lalita@ninjavan.co>
2026-07-16 15:13:46 +0700

    Comment out PODA input and related print statements
    Commented out sections related to PODA data input and print statements.

--- pipeline/jobs/day10_external_data.py
+++ pipeline/jobs/day10_external_data.py
@@ -13,14 +13,14 @@ def run():
 
     print("===== Ambil data dari RDO done =====")
 
-    print("====== Kita Kerjain Day 10 Buat PODA ya =========")
+    # print("====== Kita Kerjain Day 10 Buat PODA ya =========")
 
-    df_poda_agg = read_sheet(
-        GSHEET["poda_agg"]["sheet_id"],
-        GSHEET["poda_agg"]["tabs"]["main"]
-    )
+    # df_poda_agg = read_sheet(
+    #     GSHEET["poda_agg"]["sheet_id"],
+    #     GSHEET["poda_agg"]["tabs"]["main"]
+    # )
 
-    print("===== Ambil data dari PODA done =====")
+    # print("===== Ambil data dari PODA done =====")
 
     
     print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
@@ -102,43 +102,43 @@ def run():
     print("===== Done Input RDO ke Tracker =====")
 
 
-    print("===== Mulai input PODA ke Tracker dulu ya =====")
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_int_comp"],
-        df=df_poda_agg,
-        start_cell="P5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_int_comp"],
-        df=df_poda_agg,
-        start_cell="Z5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_int_comp"],
-        df=df_poda_agg,
-        start_cell="P5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_int_comp"],
-        df=df_poda_agg,
-        start_cell="P5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_int_comp"],
-        df=df_poda_agg,
-        start_cell="P5",
-        include_header=False
-    )
-    print("===== Done Input PODA ke Tracker =====")
+    # print("===== Mulai input PODA ke Tracker dulu ya =====")
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_int_comp"],
+    #     df=df_poda_agg,
+    #     start_cell="P5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_int_comp"],
+    #     df=df_poda_agg,
+    #     start_cell="Z5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_int_comp"],
+    #     df=df_poda_agg,
+    #     start_cell="P5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_int_comp"],
+    #     df=df_poda_agg,
+    #     start_cell="P5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_int_comp"],
+    #     df=df_poda_agg,
+    #     start_cell="P5",
+    #     include_header=False
+    # )
+    # print("===== Done Input PODA ke Tracker =====")
 
     print("===== Mulai input OVER MP ke Tracker dulu ya =====")
     write_sheet(
