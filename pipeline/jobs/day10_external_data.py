4781f87b3a6b70903d27d99afa8a0681f659716f
griyalalita-design <griya.lalita@ninjavan.co>
2026-07-16 15:31:23 +0700

    Update day10_external_data.py

--- pipeline/jobs/day10_external_data.py
+++ pipeline/jobs/day10_external_data.py
@@ -4,14 +4,14 @@ from config.settings import GSHEET
 
 
 def run():
-    print("====== Kita Kerjain Day 10 Buat RDO ya =========")
+    # print("====== Kita Kerjain Day 10 Buat RDO ya =========")
 
-    df_rdo = read_sheet(
-        GSHEET["rdo_comp"]["sheet_id"],
-        GSHEET["rdo_comp"]["tabs"]["main"]
-    )
+    # df_rdo = read_sheet(
+    #     GSHEET["rdo_comp"]["sheet_id"],
+    #     GSHEET["rdo_comp"]["tabs"]["main"]
+    # )
 
-    print("===== Ambil data dari RDO done =====")
+    # print("===== Ambil data dari RDO done =====")
 
     # print("====== Kita Kerjain Day 10 Buat PODA ya =========")
 
@@ -23,14 +23,14 @@ def run():
     # print("===== Ambil data dari PODA done =====")
 
     
-    # print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
+    print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
 
-    # df_over_mp_fte = read_sheet(
-    #     GSHEET["over_mp_fte"]["sheet_id"],
-    #     GSHEET["over_mp_fte"]["tabs"]["main"]
-    # )
+    df_over_mp_fte = read_sheet(
+        GSHEET["over_mp_fte"]["sheet_id"],
+        GSHEET["over_mp_fte"]["tabs"]["main"]
+    )
 
-    # print("===== Ambil data dari OVER MP done =====")
+    print("===== Ambil data dari OVER MP done =====")
 
     # print("====== Kita Kerjain Day 10 Buat Vol ya =========")
 
@@ -63,43 +63,43 @@ def run():
 
     
 
-    print("===== Mulai input RDO ke Tracker dulu ya =====")
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_otif"],
-        df=df_rdo,
-        start_cell="I5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_otif"],
-        df=df_rdo,
-        start_cell="I5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_otif"],
-        df=df_rdo,
-        start_cell="I5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_otif"],
-        df=df_rdo,
-        start_cell="I5",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_otif"],
-        df=df_rdo,
-        start_cell="I5",
-        include_header=False
-    )
-    print("===== Done Input RDO ke Tracker =====")
+    # print("===== Mulai input RDO ke Tracker dulu ya =====")
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_otif"],
+    #     df=df_rdo,
+    #     start_cell="I5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_otif"],
+    #     df=df_rdo,
+    #     start_cell="I5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_otif"],
+    #     df=df_rdo,
+    #     start_cell="I5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_otif"],
+    #     df=df_rdo,
+    #     start_cell="I5",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_otif"],
+    #     df=df_rdo,
+    #     start_cell="I5",
+    #     include_header=False
+    # )
+    # print("===== Done Input RDO ke Tracker =====")
 
 
     # print("===== Mulai input PODA ke Tracker dulu ya =====")
@@ -140,43 +140,43 @@ def run():
     # )
     # print("===== Done Input PODA ke Tracker =====")
 
-    # print("===== Mulai input OVER MP ke Tracker dulu ya =====")
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
-    #     df=df_over_mp_fte,
-    #     start_cell="AA6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
-    #     df=df_over_mp_fte,
-    #     start_cell="AA6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
-    #     df=df_over_mp_fte,
-    #     start_cell="AA6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
-    #     df=df_over_mp_fte,
-    #     start_cell="AA6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
-    #     df=df_over_mp_fte,
-    #     start_cell="AA6",
-    #     include_header=False
-    # )
-    # print("===== Done Input OVER MP ke Tracker =====")
+    print("===== Mulai input OVER MP ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
+        df=df_over_mp_fte,
+        start_cell="AA6",
+        include_header=False
+    )
+    print("===== Done Input OVER MP ke Tracker =====")
 
     # print("===== Mulai input vol ke Tracker dulu ya =====")
     # write_sheet(
