0976def9808120c6837195dfecfe10e770287748
griyalalita-design <griya.lalita@ninjavan.co>
2026-07-16 15:34:55 +0700

    Update day10_external_data.py

--- pipeline/jobs/day10_external_data.py
+++ pipeline/jobs/day10_external_data.py
@@ -23,14 +23,14 @@ def run():
     # print("===== Ambil data dari PODA done =====")
 
     
-    print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
+    # print("====== Kita Kerjain Day 10 Buat OVER MP ya =========")
 
-    df_over_mp_fte = read_sheet(
-        GSHEET["over_mp_fte"]["sheet_id"],
-        GSHEET["over_mp_fte"]["tabs"]["main"]
-    )
+    # df_over_mp_fte = read_sheet(
+    #     GSHEET["over_mp_fte"]["sheet_id"],
+    #     GSHEET["over_mp_fte"]["tabs"]["main"]
+    # )
 
-    print("===== Ambil data dari OVER MP done =====")
+    # print("===== Ambil data dari OVER MP done =====")
 
     # print("====== Kita Kerjain Day 10 Buat Vol ya =========")
 
@@ -42,23 +42,23 @@ def run():
     # print("===== Ambil data dari vol done =====")
 
     
-    # print("====== Kita Kerjain Day 10 Buat lm_cost ya =========")
+    print("====== Kita Kerjain Day 10 Buat lm_cost ya =========")
 
-    # df_lm_cost = read_sheet(
-    #     GSHEET["lm_cost"]["sheet_id"],
-    #     GSHEET["lm_cost"]["tabs"]["main"]
-    # )
+    df_lm_cost = read_sheet(
+        GSHEET["lm_cost"]["sheet_id"],
+        GSHEET["lm_cost"]["tabs"]["main"]
+    )
 
-    # print("===== Ambil data dari LM Cost done =====")
+    print("===== Ambil data dari LM Cost done =====")
 
-    # print("====== Kita Kerjain Day 10 Buat Hub MP ya =========")
+    print("====== Kita Kerjain Day 10 Buat Hub MP ya =========")
 
-    # df_hub_mp = read_sheet(
-    #     GSHEET["hub_mp_cost"]["sheet_id"],
-    #     GSHEET["hub_mp_cost"]["tabs"]["main"]
-    # )
+    df_hub_mp = read_sheet(
+        GSHEET["hub_mp_cost"]["sheet_id"],
+        GSHEET["hub_mp_cost"]["tabs"]["main"]
+    )
 
-    # print("===== Ambil data dari Hub MP done =====")
+    print("===== Ambil data dari Hub MP done =====")
 
 
     
@@ -140,43 +140,43 @@ def run():
     # )
     # print("===== Done Input PODA ke Tracker =====")
 
-    print("===== Mulai input OVER MP ke Tracker dulu ya =====")
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
-        df=df_over_mp_fte,
-        start_cell="AA6",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
-        df=df_over_mp_fte,
-        start_cell="AA6",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
-        df=df_over_mp_fte,
-        start_cell="AA6",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
-        df=df_over_mp_fte,
-        start_cell="AA6",
-        include_header=False
-    )
-    write_sheet(
-        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
-        df=df_over_mp_fte,
-        start_cell="AA6",
-        include_header=False
-    )
-    print("===== Done Input OVER MP ke Tracker =====")
+    # print("===== Mulai input OVER MP ke Tracker dulu ya =====")
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
+    #     df=df_over_mp_fte,
+    #     start_cell="AA6",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
+    #     df=df_over_mp_fte,
+    #     start_cell="AA6",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
+    #     df=df_over_mp_fte,
+    #     start_cell="AA6",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
+    #     df=df_over_mp_fte,
+    #     start_cell="AA6",
+    #     include_header=False
+    # )
+    # write_sheet(
+    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
+    #     df=df_over_mp_fte,
+    #     start_cell="AA6",
+    #     include_header=False
+    # )
+    # print("===== Done Input OVER MP ke Tracker =====")
 
     # print("===== Mulai input vol ke Tracker dulu ya =====")
     # write_sheet(
@@ -216,81 +216,81 @@ def run():
     # )
     # print("===== Done Input Vol ke Tracker =====")
 
-    # print("===== Mulai input LM Cost ke Tracker dulu ya =====")
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
-    #     df=df_lm_cost,
-    #     start_cell="H6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
-    #     df=df_lm_cost,
-    #     start_cell="H6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
-    #     df=df_lm_cost,
-    #     start_cell="H6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
-    #     df=df_lm_cost,
-    #     start_cell="H6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
-    #     df=df_lm_cost,
-    #     start_cell="H6",
-    #     include_header=False
-    # )
-    # print("===== Done Input LM Cost ke Tracker =====")
+    print("===== Mulai input LM Cost ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
+        df=df_lm_cost,
+        start_cell="H6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
+        df=df_lm_cost,
+        start_cell="H6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
+        df=df_lm_cost,
+        start_cell="H6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
+        df=df_lm_cost,
+        start_cell="H6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
+        df=df_lm_cost,
+        start_cell="H6",
+        include_header=False
+    )
+    print("===== Done Input LM Cost ke Tracker =====")
 
-    # print("===== Mulai input Hub MP Cost ke Tracker dulu ya =====")
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
-    #     df=df_hub_mp,
-    #     start_cell="O6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
-    #     df=df_hub_mp,
-    #     start_cell="O6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
-    #     df=df_hub_mp,
-    #     start_cell="O6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
-    #     df=df_hub_mp,
-    #     start_cell="O6",
-    #     include_header=False
-    # )
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
-    #     df=df_hub_mp,
-    #     start_cell="O6",
-    #     include_header=False
-    # )
-    # print("===== Done Input LM Cost ke Tracker =====")
+    print("===== Mulai input Hub MP Cost ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_gj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_gj"]["tabs"]["raw_data_cost"],
+        df=df_hub_mp,
+        start_cell="O6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_sum"]["sheet_id"],
+        sheet_name=GSHEET["tracker_sum"]["tabs"]["raw_data_cost"],
+        df=df_hub_mp,
+        start_cell="O6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_wj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_wj"]["tabs"]["raw_data_cost"],
+        df=df_hub_mp,
+        start_cell="O6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_cj"]["sheet_id"],
+        sheet_name=GSHEET["tracker_cj"]["tabs"]["raw_data_cost"],
+        df=df_hub_mp,
+        start_cell="O6",
+        include_header=False
+    )
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker_ej"]["sheet_id"],
+        sheet_name=GSHEET["tracker_ej"]["tabs"]["raw_data_cost"],
+        df=df_hub_mp,
+        start_cell="O6",
+        include_header=False
+    )
+    print("===== Done Input LM Cost ke Tracker =====")
 
 if __name__ == "__main__":
     run()
